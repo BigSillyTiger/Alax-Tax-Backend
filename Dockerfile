6bf61e62e835f74cf898a1e170a1ddeaaf00511b
Areos Chen <areos.cc@gmail.com>
2024-10-30 23:44:31 +1030

    original fix

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,15 @@
-FROM node:alpine as build
+FROM node:alpine AS build
 
 WORKDIR /app
+
 COPY package*.json ./
 
 RUN npm install
-COPY . .
-RUN npm run build
 
-FROM build as production
-# set env
+COPY . .
+
 ENV NODE_ENV=production
-WORKDIR /app
-COPY --from=build /app/dist ./dist
+
 EXPOSE 8080
+
 CMD ["npm", "start"]
--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,8 +1,8 @@
 const allowedOrigins = [
     "http://localhost:5000",
     //"https://cpsoftware.com.au",
-    "https://test.cpsoftware.com.au",
+    //"https://test.cpsoftware.com.au",
     //"https://www.test.cpsoftware.com.au",
-    //"https://tool.srclandscaping.com.au",
+    "https://tool.srclandscaping.com.au",
 ];
 export default allowedOrigins;
